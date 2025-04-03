d36ed61e37a6704e449412c607757048daf33bea
h0angpc <118001661+h0angpc@users.noreply.github.com>
2025-04-03 22:10:07 +0700

    add: add product-api

---
+++ src/main/java/com/mongodb/starter/repositories/interfaces/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.mongodb.starter.repositories.interfaces;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.mongodb.starter.entity.ProductEntity;
+
+@Repository
+public interface ProductRepository {
+    
+    ProductEntity insertOne(ProductEntity productEntity);
+    
+    void deleteOne(String id);
+    
+    List<ProductEntity> findAll();
+    
+    ProductEntity findOne(String id);
+    
+    ProductEntity updateOne(ProductEntity entity);
+}
---
+++ src/main/java/com/mongodb/starter/usecases/implement/ProductUsecaseImpl.java
@@ -0,0 +1,43 @@
+package com.mongodb.starter.usecases.implement;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+import com.mongodb.starter.entity.ProductEntity;
+import com.mongodb.starter.repositories.interfaces.ProductRepository;
+import com.mongodb.starter.usecases.interfaces.ProductUsecase;
+
+@Service
+public class ProductUsecaseImpl implements ProductUsecase {
+
+    private final ProductRepository productRepository;
+
+    public ProductUsecaseImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public ProductEntity createProduct(ProductEntity productEntity) {
+        return productRepository.insertOne(productEntity);
+    }
+
+    @Override
+    public List<ProductEntity> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public ProductEntity getProduct(String id) {
+        return productRepository.findOne(id);
+    }
+
+    @Override
+    public ProductEntity updateProduct(ProductEntity entity) {
+        return productRepository.updateOne(entity);
+    }
+
+    @Override
+    public void deleteProduct(String id) {
+        productRepository.deleteOne(id);
+    }
+}
